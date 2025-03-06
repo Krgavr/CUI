f4a502e9659949da7954ae4a70744a9542d7397e
krgavr <kr.gavr03@gmail.com>
2025-03-06 12:37:53 +0100

    Přidejte soubory projektu.

---
+++ WPF_cviceni2/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using WPF_cviceni2.Models;
+using WPF_cviceni2.Services;
+
+namespace WPF_cviceni2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+
+    private readonly WeatherService weatherService;
+    private WeatherModel weather;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        this.weatherService = new WeatherService();
+
+    }
+
+    private void UpdateSunPosition(long sunriseUnix, long sunsetUnix, long currentUnix)
+    {
+        bool isDaytime = currentUnix > sunriseUnix && currentUnix < sunsetUnix;
+
+        if (!isDaytime)
+        {
+            DayLine.Stroke = new SolidColorBrush(Color.FromRgb(170, 170, 200));
+            Sun.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    private async void Search_Click(object sender, RoutedEventArgs e)
+    {
+        string city = CityName.Text;
+
+        try
+        {
+            weather = await weatherService.GetWeatherInfo(city);
+
+            CityNameText.Text = weather.name;
+            TemperatureText.Text = $"Temp: {weather.main.temp}";
+            FeelsLikeText.Text = $"Feels like: {weather.main.feelsLike}";
+            HumidityText.Text = $"Humidity: {weather.main.humidity}";
+
+            string currentTime = weatherService.ConvertTime(weather.datetime, weather.timezoneOffset);
+            CurrentTimeText.Text = $"Current time in {weather.name}: {currentTime}";
+
+            UpdateSunPosition(weather.sys.sunrise, weather.sys.sunset, weather.datetime);
+
+            SunCanvas.Visibility = Visibility.Visible;
+
+            string sunriseTime = weatherService.ConvertTime(weather.sys.sunrise, weather.timezoneOffset);
+            SunriseText.Text = sunriseTime;
+        }
+        catch(Exception ex)
+        {
+            Trace.WriteLine("Error occured!");
+        }
+    }
+
+    public void Save_Click(object sender, RoutedEventArgs e)
+    {
+        if (weather == null)
+        {
+            Trace.WriteLine("Error: weather object is null!");
+            return;
+        }
+
+
+        try
+        {
+            string json = JsonSerializer.Serialize(weather, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("weatherData.json", json);
+            Trace.WriteLine("Weather data saved successfully!");
+        }
+        catch (Exception ex )
+        {
+            Trace.WriteLine("Could not be saved.");
+        }
+    }
+}
---
+++ WPF_cviceni2/Services/WeatherService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using WPF_cviceni2.Models;
+
+namespace WPF_cviceni2.Services 
+{
+    class WeatherService
+    {
+        private readonly HttpClient _client;
+        private const string API_KEY = "add api key ";
+        private const string BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric";
+        public WeatherService()
+        {
+            this._client = new HttpClient();
+        }
+
+        public async Task<WeatherModel> GetWeatherInfo(string city)
+        {
+            string url = string.Format(BASE_URL, city, API_KEY);
+
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed the request!");
+            }
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonResponse));
+            JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+
+            WeatherModel weather = await JsonSerializer.DeserializeAsync<WeatherModel>(stream, options);
+
+            return weather;
+        }
+
+        public string ConvertTime(long datetime, int offset)
+        {
+            DateTimeOffset utcOffset = DateTimeOffset.FromUnixTimeSeconds(datetime);
+
+            TimeSpan timeSpan = TimeSpan.FromSeconds(offset);
+            DateTimeOffset localDateTime = utcOffset.ToOffset(timeSpan);
+
+            string formattedTime = localDateTime.ToString("HH:mm");
+
+            return formattedTime;
+        }
+
+    }
+}
